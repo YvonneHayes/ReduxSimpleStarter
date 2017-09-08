99594fd50ed8d47e56210ea0864159817e8e7e7d
Yvonne Hayes <yvonne.hayes@skyward.io>
2017-09-08 10:48:07 -0700

    added components folder and component files. Also added youtube search package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyBdBeO-YfrzZftWXvHwtHnGRwqVdE3pX0w';
+
 // Create a new component. This component should produce some HTML
 
-const App = function() {
-  return <div>Hi!</div>;
+const App = () => {
+  return <div>Hello, world!!!</div>;
 }
 
-
 // Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));

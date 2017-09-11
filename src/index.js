66323904c401244083689f092f5bb2b2e57b8d82
Yvonne Hayes <yvonne.hayes@skyward.io>
2017-09-11 15:39:03 -0700

    main video view available. Can search videos and click different videos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-import YTSearch from'youtube-api-search';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBdBeO-YfrzZftWXvHwtHnGRwqVdE3pX0w';
 
@@ -12,19 +13,30 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = {videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('french bulldog');
+  }
 
-    YTSearch({key:API_KEY, term: 'french bulldog'}, (videos) => {
-      this.setState({videos});
-      //this.setState({videos:videos}); ES6 shorthand above key and property have to be identical for this to work
+  videoSearch(term) {
+    YTSearch({key:API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+         onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+         videos={this.state.videos} />
       </div>
     );
   }
